75ca3cf777b2f36b72daac92ca243f66fa6f9e5b
nidolight <nidolight@outlook.com>
2022-07-03 04:43:57 +0900

    landing page, about_me page added

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('<int:pk>/', views.single_post_page),
     path('', views.index),
 ]
