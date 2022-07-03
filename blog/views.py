cbc0503be62973d13bbb46e4bb8363f3d7ce5a19
nidolight <nidolight@outlook.com>
2022-07-04 07:43:14 +0900

    tag page feature  updated

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Post, Category
+from .models import Post, Category, Tag
+
 
 class PostList(ListView):
     model = Post
@@ -13,6 +14,7 @@ class PostList(ListView):
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
         return context
 
+
 class PostDetail(DetailView):
     model = Post
 
@@ -21,6 +23,7 @@ class PostDetail(DetailView):
         context['categories'] = Category.objects.all()
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
         return context
+
 
 def category_page(request,slug):
     if slug == 'no_category':
@@ -38,6 +41,22 @@ def category_page(request,slug):
             'categories': Category.objects.all(),
             'no_category_post_count': Post.objects.filter(category=None).count(),
             'category': category,
+        }
+    )
+
+
+def tag_page(request, slug):
+    tag = Tag.objects.get(slug=slug)
+    post_list = tag.post_set.all()
+
+    return render(
+        request,
+        'blog/post_list.html',
+        {
+            'post_list': post_list,
+            'tag': tag,
+            'categories': Category.objects.all(),
+            'no_category_post_count': Post.objects.filter(category=None).count(),
         }
     )
 
