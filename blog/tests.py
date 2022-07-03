35dc73d1141ab98d2e9d71469ec9a1f43d7a0381
nidolight <nidolight@outlook.com>
2022-07-04 02:44:51 +0900

    navbar, footer modulized

--- blog/tests.py
+++ blog/tests.py
@@ -6,6 +6,23 @@ class TestView(TestCase):
     def setUp(self):
         self.client = Client() # Client는 장고에서 테스트를 위한 가상의 사용자
 
+    def navbar_test(self, soup):
+        navbar = soup.nav
+        self.assertIn('Blog', navbar.text)
+        self.assertIn('About Me', navbar.text)
+
+        logo_btn = navbar.find('a', text='Do It Django')
+        self.assertEqual(logo_btn.attrs['href'], '/')
+
+        home_btn = navbar.find('a', text='Home')
+        self.assertEqual(home_btn.attrs['href'], '/')
+
+        blog_btn = navbar.find('a', text='Blog')
+        self.assertEqual(blog_btn.attrs['href'], '/blog/')
+
+        about_me_btn = navbar.find('a', text='About Me')
+        self.assertEqual(about_me_btn.attrs['href'], '/about_me/')
+
     def test_post_list(self):
         # 1.1 포스트 목록 페이지 가져온다.
         response = self.client.get('/blog/')
@@ -17,12 +34,13 @@ class TestView(TestCase):
         soup = BeautifulSoup(response.content, 'html.parser')
         self.assertEqual(soup.title.text, 'Blog')
 
-        # 1.4 내비게이션 바가 있다.
-        navbar = soup.nav
-
-        # 1.5 Blog, Aout Me라는 문구가 내비게이션 바에 있다.
-        self.assertIn('Blog', navbar.text)
-        self.assertIn('About Me', navbar.text)
+        # # 1.4 내비게이션 바가 있다.
+        # navbar = soup.nav
+        #
+        # # 1.5 Blog, Aout Me라는 문구가 내비게이션 바에 있다.
+        # self.assertIn('Blog', navbar.text)
+        # self.assertIn('About Me', navbar.text)
+        self.navbar_test(soup)
 
         # 2.1 메인 영역에 게시물이 하나도 없다면
         # (TestCase에서는 새로운 DB를 따로 만들어 사용하므로 비어있다.)
@@ -57,7 +75,7 @@ class TestView(TestCase):
         # 3.4 ' 아직 게시물이 없습니다' 라는 문구는 더이상 보이지 않는다.
         self.assertNotIn('아직 게시물이 없습니다', main_area.text)
 
-    def test_post_detail(self):\
+    def test_post_detail(self):
         # 1.1 포스트가 하나 있다.
         post_001 = Post.objects.create(
             title='첫 번째 포스트입니다.',
@@ -73,10 +91,11 @@ class TestView(TestCase):
         self.assertEqual(response.status_code, 200)
         soup = BeautifulSoup(response.content, 'html.parser')
 
-        # 2.2 포스트 목록 페이지와 똑같은 내비게이션 바가 있다.
-        navbar = soup.nav
-        self.assertIn('Blog', navbar.text)
-        self.assertIn('About Me', navbar.text)
+        # # 2.2 포스트 목록 페이지와 똑같은 내비게이션 바가 있다.
+        # navbar = soup.nav
+        # self.assertIn('Blog', navbar.text)
+        # self.assertIn('About Me', navbar.text)
+        self.navbar_test(soup)
 
         # 2.3 첫 번째 포스트의 제목이 웹 브라우저 탭 타이틀에 들어 있다.
         self.assertIn(post_001.title, soup.title.text)
@@ -91,3 +110,6 @@ class TestView(TestCase):
 
         # 2.6 첫 번째 포스트의 내용이 포스트 영역에 있다.
         self.assertIn(post_001.content, post_area.text)
+
+
+
