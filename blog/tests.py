da9ae2b3a745bbbf185d348c6b046f4e133016f1
nidolight <nidolight@outlook.com>
2022-07-04 01:28:11 +0900

    testing post_list when there is no posts

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,51 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from bs4 import BeautifulSoup
+from .models import Post
 
-# Create your tests here.
+class TestView(TestCase):
+    def setUp(self):
+        self.client = Client() # Client는 장고에서 테스트를 위한 가상의 사용자
+
+    def test_post_list(self):
+        # 1.1 포스트 목록 페이지 가져온다.
+        response = self.client.get('/blog/')
+        # 1.2 정상적으로 페이지가 로드 된다.
+        self.assertEqual(response.status_code, 200)
+        # 1.3 페이지 타이틀은 'Blog'다.
+        soup = BeautifulSoup(response.content, 'html.parser')
+        self.assertEqual(soup.title.text, 'Blog')
+        # 1.4 내비게이션 바가 있다.
+        navbar = soup.nav
+        # 1.5 Blog, Aout Me라는 문구가 내비게이션 바에 있다.
+        self.assertIn('Blog', navbar.text)
+        self.assertIn('About Me', navbar.text)
+
+        # 2.1 메인 영역에 게시물이 하나도 없다면
+        # (TestCase에서는 새로운 DB를 따로 만들어 사용하므로 비어있다.)
+        self.assertEqual(Post.objects.count(), 0)
+        # 2.2 '아직 게시물이 없습니다'라는 문구 출력
+        main_area = soup.find('div', id='main-area')
+        self.assertIn('아직 게시물이 없습니다.', main_area.text)
+
+        # 3.1 게시물이 2개 이상이라면
+        # (비어있으므로 게시물 2개를 만들어준다.)
+        post_001 = Post.objects.create(
+            title='첫 번째 포스트입니다.',
+            content='Hello World!!!',
+        )
+        post_002 = Post.objects.create(
+            title='두 번째 포스트입니다.',
+            content='Nice to meet you.',
+        )
+        self.assertEqual(Post.objects.count(), 2)
+
+        # 3.2 포스트 목록 페이지를 새로고침했을 때
+        response = self.client.get('/blog/')
+        soup = BeautifulSoup(response.content, 'html.parser')
+        self.assertEqual(response.status_code, 200)
+        # 3.3 메인 영역에 포스트 2개의 타이틀이 존재한다.
+        main_area = soup.find('div', id='main-area')
+        self.assertIn(post_001.title, main_area.text)
+        self.assertIn(post_002.title, main_area.text)
+        # 3.4 ' 아직 게시물이 없습니다' 라는 문구는 더이상 보이지 않는다.
+        self.assertNotIn('아직 게시물이 없습니다', main_area.text)
